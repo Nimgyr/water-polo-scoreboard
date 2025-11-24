720b0687c3085b436b05fd37ebe39fe4c735f6f5
Nimgyr <maxim.murmantsev@gmail.com>
2025-11-24 14:21:53 +0400

    Initial commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,15 @@
+function App(): React.JSX.Element {
+  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <>
+      <div className="action">
+        <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
+          Send IPC
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
